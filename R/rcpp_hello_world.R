1e45c5b76eb45919293291ae3505cf86b6389269
muelleki <kirill.mueller@ivt.baug.ethz.ch>
2013-03-05 09:25:33 +0100

    Roxygenize

--- R/rcpp_hello_world.R
+++ R/rcpp_hello_world.R
@@ -1,3 +1,13 @@
+#'@useDynLib wrswoR
+
+#'@title Simple function using Rcpp
+#'@name rcpp_hello_world
+#'@examples
+#'
+#'\dontrun{
+#'rcpp_hello_world()
+#'}
+#'
 rcpp_hello_world <-
 function () 
 {
---
+++ R/wrswoR-package.R
@@ -0,0 +1,31 @@
+#'@exportPattern "^[[:alpha:]]+"
+
+
+#'@title What the package does (short line)
+#'@description More about what it does (maybe more than one line) ~~ A
+#'  concise (1-5 lines) description of the package ~~
+#'@details
+#'  \tabular{ll}{ Package: \tab wrswoR\cr Type: \tab Package\cr Version:
+#'  \tab 1.0\cr Date: \tab 2013-03-05\cr License: \tab What license is
+#'  it under?\cr } ~~ An overview of how to use the package, including
+#'  the most important functions ~~
+#'  
+#'@name wrswoR-package
+#'@aliases wrswoR-package wrswoR
+#'@docType package
+#'@author Who wrote it
+#'  
+#'  Maintainer: Who to complain to <yourfault@@somewhere.net>
+#'@seealso ~~ Optional links to other man pages, e.g. ~~ ~~ 
+#'  \code{\link[<pkg>:<pkg>-package]{<pkg>}} ~~
+#'@references ~~ Literature or other references for background
+#'  information ~~
+#'@keywords package
+#'@examples
+#'
+#'%% ~~ simple examples of the most important functions ~~
+#'
+NULL
+
+
+
