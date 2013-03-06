7c71cf9406e30f1d1e8d77719dc2d589dfbebf45
muelleki <kirill.mueller@ivt.baug.ethz.ch>
2013-03-06 10:45:20 +0100

    comment

--- src/sample_int_crank.cpp
+++ src/sample_int_crank.cpp
@@ -14,10 +14,19 @@ T _add_one(T t) { return t + 1; }
 
 // [[Rcpp::export(sample.int.crank)]]
 IntegerVector sample_int_crank(int n, int size, NumericVector prob) {
+  // We need the last "size" elements of
+  // U ^ (1 / prob) ~ log(U) / prob
+  //                ~ -Exp(1) / prob
+  //                ~ prob / Exp(1)
+  // Here, ~ means "doesn't change order statistics".
   NumericVector rnd = prob / rexp(n);
+  
+  // Find the indexes of the first "size" elements under inverted
+  // comparison.  Here, vx is zero-based.
   IntegerVector vx = seq(0, n - 1);
   std::partial_sort(vx.begin(), vx.begin() + size, vx.end(), Comp(rnd));
   
-  // Initialize with elements vx[1:size], applying transform "+ 1"
+  // Initialize with elements vx[1:size], applying transform "+ 1" --
+  // we return one-based values.
   return IntegerVector(vx.begin(), vx.begin() + size, &_add_one<int>);
 }
