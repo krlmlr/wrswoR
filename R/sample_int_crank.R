9c0fa6cf982b798d3f58fe122d83b5dd9ba2558f
muelleki <kirill.mueller@ivt.baug.ethz.ch>
2013-03-06 10:03:36 +0100

    initial C++ version as implemented by Julius

--- R/sample_int_crank.R
+++ R/sample_int_crank.R
@@ -1,43 +1,44 @@
-#' @title Weighted sampling without replacement using reservoir sampling
-#'   (Rcpp version)
-#' @description \code{sample.int.crank} takes a sample of the specified 
-#'   \code{size} from the elements of \code{1:n} without replacement. 
-#'   This function is faster than \code{sample.int} in many cases, 
-#'   especially when \code{n} and \code{size} are large, even if the 
-#'   weights range over many orders of magnitudes.
-#' @inheritParams base::sample.int
-#' @return An integer vector of length \code{size} with elements from 
-#'   \code{1:n}.
-#' @details The call \code{sample.int.crank(n, size, prob)} is equivalent
-#'   to \code{sample.int(n, size, replace=F, prob)}.  (The results will 
-#'   most probably be different for the same random seed, but the 
-#'   returned samples are distributed identically for both calls.) 
-#'   However, \code{sample.int} implements an algorithm with quadratic 
-#'   runtime -- this is not suitable for large values of \code{n} and 
-#'   \code{size} (see also 
-#'   \url{http://stackoverflow.com/q/15113650/946850}).  (Note that the 
-#'   performance of \code{sample.int} is just fine in the "with 
-#'   replacement" case, and also for uniform probabilities.)
-#'   
-#'   This function implements a variant of reservoir sampling. It can be
-#'   shown that the order statistic of U^(1/w_i) has the same 
-#'   distribution as random sampling without replacement (U=uniform 
-#'   (0,1) distribution). To increase numerical stability, log(U) / w_i 
-#'   is computed instead; the log transform does not change the order 
-#'   statistic.
-#' @author Kirill Müller
-#' @references Pavlos S. Efraimidis, Paul G. Spirakis, Weighted random 
-#'   sampling with a reservoir, Information Processing Letters, Volume 
-#'   97, Issue 5, 16 March 2006, Pages 181-185, ISSN 0020-0190, 
-#'   10.1016/j.ipl.2005.11.003
+#'@title Weighted sampling without replacement using reservoir sampling 
+#'  (Rcpp version)
+#'@description \code{sample.int.crank} takes a sample of the specified 
+#'  \code{size} from the elements of \code{1:n} without replacement. 
+#'  This function is faster than \code{sample.int} in many cases, 
+#'  especially when \code{n} and \code{size} are large, even if the 
+#'  weights range over many orders of magnitudes.
+#'@inheritParams base::sample.int
+#'@return An integer vector of length \code{size} with elements from 
+#'  \code{1:n}.
+#'@details The call \code{sample.int.crank(n, size, prob)} is equivalent
+#'  to \code{sample.int(n, size, replace=F, prob)}.  (The results will 
+#'  most probably be different for the same random seed, but the 
+#'  returned samples are distributed identically for both calls.) 
+#'  However, \code{sample.int} implements an algorithm with quadratic 
+#'  runtime -- this is not suitable for large values of \code{n} and 
+#'  \code{size} (see also 
+#'  \url{http://stackoverflow.com/q/15113650/946850}).  (Note that the 
+#'  performance of \code{sample.int} is just fine in the "with 
+#'  replacement" case, and also for uniform probabilities.)
+#'  
+#'  This function implements a variant of reservoir sampling. It can be 
+#'  shown that the order statistic of U^(1/w_i) has the same 
+#'  distribution as random sampling without replacement (U=uniform (0,1)
+#'  distribution). To increase numerical stability, log(U) / w_i is 
+#'  computed instead; the log transform does not change the order 
+#'  statistic.
+#'@author Julius
+#'@author Kirill Müller
+#'@author Dinre
+#'@references Efraimidis, Pavlos S., and Paul G. Spirakis. "Weighted 
+#'  random sampling with a reservoir." \emph{Information Processing 
+#'  Letters} 97, no. 5 (2006): 181-185.
 #' @examples
-#' s <- sample.int.rank(200000, 100000, runif(200000))
+#' s <- sample.int.crank(200000, 100000, runif(200000))
 #' stopifnot(unique(s) == s)
 #' p <- c(995, rep(1, 5))
 #' n <- 1000
 #' set.seed(42)
 #' stopifnot(abs(table(replicate(sample.int.crank(6, 3, p), n=n)) / n -
 #'   c(1, rep(0.4, 5))) < 0.04)
-#' @seealso \code{\link[base]{sample.int}}
+#'@seealso \code{\link[base]{sample.int}}
 sample.int.crank <- function(n, size, prob) {
 }
--- src/sample_int_crank.cpp
+++ src/sample_int_crank.cpp
@@ -2,7 +2,20 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
+struct Comp{
+  Comp(const Rcpp::NumericVector& v ) : _v(v) {}
+  bool operator ()(int a, int b) { return _v[a] < _v[b]; }
+  const Rcpp::NumericVector& _v;
+};
+
 // [[Rcpp::export(sample.int.crank)]]
 IntegerVector sample_int_crank(int n, int size, NumericVector prob) {
-    return IntegerVector(size);
+    int num = size, x = n;
+    Rcpp::IntegerVector vx = Rcpp::clone<Rcpp::IntegerVector>(x);
+    Rcpp::NumericVector pr = Rcpp::clone<Rcpp::NumericVector>(prob);
+    Rcpp::NumericVector rnd = rexp(x) / pr;
+    for (int i= 0; i<vx.size(); ++i) vx[i] = i;
+    std::partial_sort(vx.begin(), vx.begin() + num, vx.end(), Comp(rnd));
+    vx = vx[seq(0, num - 1)] + 1;
+    return vx;
 }
