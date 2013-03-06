29c5f141268f58015662e9e72a4d4d0688f5e756
muelleki <kirill.mueller@ivt.baug.ethz.ch>
2013-03-06 10:06:39 +0100

    remove aliases

--- src/sample_int_crank.cpp
+++ src/sample_int_crank.cpp
@@ -10,12 +10,9 @@ struct Comp{
 
 // [[Rcpp::export(sample.int.crank)]]
 IntegerVector sample_int_crank(int n, int size, NumericVector prob) {
-    int num = size, x = n;
-    Rcpp::IntegerVector vx = Rcpp::clone<Rcpp::IntegerVector>(x);
-    Rcpp::NumericVector pr = Rcpp::clone<Rcpp::NumericVector>(prob);
-    Rcpp::NumericVector rnd = rexp(x) / pr;
-    for (int i= 0; i<vx.size(); ++i) vx[i] = i;
-    std::partial_sort(vx.begin(), vx.begin() + num, vx.end(), Comp(rnd));
-    vx = vx[seq(0, num - 1)] + 1;
-    return vx;
+    Rcpp::NumericVector rnd = rexp(n) / prob;
+    Rcpp::IntegerVector vx = Rcpp::clone<Rcpp::IntegerVector>(n);
+    for (int i = 0; i<vx.size(); ++i) vx[i] = i;
+    std::partial_sort(vx.begin(), vx.begin() + size, vx.end(), Comp(rnd));
+    return vx[seq(0, size - 1)] + 1;
 }
