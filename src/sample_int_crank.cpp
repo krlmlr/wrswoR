fb7223b7ac7d33bd9953df2f048400f12bd44191
muelleki <kirill.mueller@ivt.baug.ethz.ch>
2013-03-06 10:11:48 +0100

    inverted comparison

--- src/sample_int_crank.cpp
+++ src/sample_int_crank.cpp
@@ -4,13 +4,14 @@ using namespace Rcpp;
 
 struct Comp{
   Comp(const Rcpp::NumericVector& v ) : _v(v) {}
-  bool operator ()(int a, int b) { return _v[a] < _v[b]; }
+  // Inverted comparison!
+  bool operator ()(int a, int b) { return _v[a] > _v[b]; }
   const Rcpp::NumericVector& _v;
 };
 
 // [[Rcpp::export(sample.int.crank)]]
 IntegerVector sample_int_crank(int n, int size, NumericVector prob) {
-  Rcpp::NumericVector rnd = rexp(n) / prob;
+  Rcpp::NumericVector rnd = prob / rexp(n);
   Rcpp::IntegerVector vx = seq(0, n - 1);
   std::partial_sort(vx.begin(), vx.begin() + size, vx.end(), Comp(rnd));
   return vx[seq(0, size - 1)] + 1;
