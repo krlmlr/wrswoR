b09a2731f59c2ea1897448e48d08892bd0bbdafa
muelleki <kirill.mueller@ivt.baug.ethz.ch>
2013-03-06 10:44:48 +0100

    don't use namespaces

--- src/sample_int_crank.cpp
+++ src/sample_int_crank.cpp
@@ -14,8 +14,8 @@ T _add_one(T t) { return t + 1; }
 
 // [[Rcpp::export(sample.int.crank)]]
 IntegerVector sample_int_crank(int n, int size, NumericVector prob) {
-  Rcpp::NumericVector rnd = prob / rexp(n);
-  Rcpp::IntegerVector vx = seq(0, n - 1);
+  NumericVector rnd = prob / rexp(n);
+  IntegerVector vx = seq(0, n - 1);
   std::partial_sort(vx.begin(), vx.begin() + size, vx.end(), Comp(rnd));
   
   // Initialize with elements vx[1:size], applying transform "+ 1"
