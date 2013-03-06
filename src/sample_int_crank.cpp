d12f8309d27a9d3ff1e5bdb4a5d80df445c048c1
muelleki <kirill.mueller@ivt.baug.ethz.ch>
2013-03-06 10:28:36 +0100

    step towards partial copying

--- src/sample_int_crank.cpp
+++ src/sample_int_crank.cpp
@@ -14,5 +14,6 @@ IntegerVector sample_int_crank(int n, int size, NumericVector prob) {
   Rcpp::NumericVector rnd = prob / rexp(n);
   Rcpp::IntegerVector vx = seq(0, n - 1);
   std::partial_sort(vx.begin(), vx.begin() + size, vx.end(), Comp(rnd));
-  return vx[seq(0, size - 1)] + 1;
+  IntegerVector vxp = vx + 1;
+  return IntegerVector(vxp.begin(), vxp.begin() + size);
 }
