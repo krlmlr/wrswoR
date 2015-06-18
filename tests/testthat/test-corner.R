aa4cd7ba4102f0c42253937d0e100bf3119455e0
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-06-18 17:21:12 +0200

    more simple tests

--- tests/testthat/test-corner.R
+++ tests/testthat/test-corner.R
@@ -6,9 +6,24 @@ test_that("zero-length output", {
   }
 })
 
-test_that("heavily skewed probability", {
-  prob <- c(1, 1e10)
+test_that("uniform probability", {
+  n <- 13
+  prob <- rep(1, n)
   for (funcname in funcnames) {
-    expect_equal(funcs[[funcname]](length(prob), 1, prob), 2, label = funcname)
+    expect_equal(sort(funcs[[funcname]](n, n, prob)), seq_len(n), label = funcname)
+  }
+})
+
+test_that("heavily skewed probability", {
+  prob <- c(rep(1, 10), 1e10)
+  for (funcname in funcnames) {
+    expect_equal(funcs[[funcname]](length(prob), 1, prob), length(prob), label = funcname)
+  }
+})
+
+test_that("heavily skewed probability (reversed)", {
+  prob <- c(1e10, rep(1, 10))
+  for (funcname in funcnames) {
+    expect_equal(funcs[[funcname]](length(prob), 1, prob), 1, label = funcname)
   }
 })
