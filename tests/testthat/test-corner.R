a45c33630b6724bf5ce09e4bb6c18f9894bfb50b
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-06-18 15:11:44 +0200

    add test at least for corner case

--- tests/testthat/test-corner.R
+++ tests/testthat/test-corner.R
@@ -3,3 +3,11 @@ context("corner")
 test_that("zero-length output", {
   expect_equal(sample.int.crank(10, 0, 1:10), integer())
 })
+
+test_that("heavily skewed probability", {
+  prob <- c(1, 1e10)
+  expect_equal(sample.int(length(prob), 1, replace = FALSE, prob = prob), 2)
+  expect_equal(sample.int.crank(length(prob), 1, prob = prob), 2)
+  expect_equal(sample.int.rank(length(prob), 1, prob = prob), 2)
+  expect_equal(sample.int.rej(length(prob), 1, prob = prob), 2)
+})
