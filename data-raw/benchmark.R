c20bef3dff51cc69c48dbfc0844f2ac4001a36f2
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-06-19 14:25:36 +0200

    extract funcion

---
+++ data-raw/benchmark.R
@@ -0,0 +1,27 @@
+.benchmark <- function() {
+  plyr::ldply(
+    PROB,
+    function(probf) {
+      plyr::ldply(
+        N,
+        function(n) {
+          prob <- probf(n)
+          plyr::ldply(
+            R,
+            function(r) {
+              s <- trunc(n * r)
+              microbenchmark::microbenchmark(
+                ccrank=sample.int.crank(n, s, prob),
+                crank=sample.int.crank(n, s, prob),
+                rank=sample.int.rank(n, s, prob),
+                rej=sample.int.rej(n, s, prob),
+                R=sample.int.R(n, s, prob),
+                control = list(order = "block", warmup = 10)
+              )
+            }) %>%
+            dplyr::mutate(r = as.numeric(.id), .id = NULL)
+        }) %>%
+        dplyr::mutate(n = as.integer(.id), .id = NULL)
+    }) %>%
+    dplyr::rename(prob = .id)
+}
