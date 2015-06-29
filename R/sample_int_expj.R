a1c12c6be9d4a45839993e24c5141d17ce4d9457
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-06-29 20:30:01 +0200

    document breakage

--- R/sample_int_expj.R
+++ R/sample_int_expj.R
@@ -7,12 +7,14 @@
 #'   \code{*_expjs} implements the algorithm in the paper verbatim
 #'   (at the cost of numerical stability). Both implementations exhibit similar
 #'   deficiencies with \code{size=2} and non-uniform weights.
+#'   Furthermore, with large inputs, the functions cause a segfault.
 #' @author Kirill Müller (for \code{_*expj*})
 #' @references Efraimidis, Pavlos S., and Paul G. Spirakis. "Weighted
 #' random sampling with a reservoir." \emph{Information Processing
 #' Letters} 97, no. 5 (2006): 181-185.
 #' @examples
 #' ## Algorithm A-ExpJ (with log-transformed keys)
+#' \dontrun{
 #' s <- sample_int_expj(200000, 100000, runif(200000))
 #' stopifnot(unique(s) == s)
 #' p <- c(995, rep(1, 5))
@@ -20,4 +22,5 @@
 #' set.seed(42)
 #' stopifnot(abs(table(replicate(sample_int_expjs(6, 3, p), n=n)) / n -
 #'   c(1, rep(0.4, 5))) < 0.04)
+#' }
 "sample_int_expj"
