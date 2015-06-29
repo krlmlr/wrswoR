2c852cd5403c0f01affb021a4cc3a7c73ca7f6eb
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-06-29 16:48:28 +0200

    rename test topic

--- tests/testthat/test-unique.R
+++ tests/testthat/test-unique.R
@@ -1,6 +1,6 @@
 context("unique")
 
-test_that("sample larger than population", {
+test_that("no dupes", {
   for (funcname in funcnames) {
     for (i in 1:20) {
       expect_equal(sum(duplicated(funcs[[funcname]](i, i - 1, seq_len(i)))), 0,
