dae3b5b2716f2190cc8ae8a0ec12e1c164a1e176
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-08-13 13:41:58 +0200

    explicitly test results

--- tests/testthat/test-unique.R
+++ tests/testthat/test-unique.R
@@ -8,3 +8,12 @@ test_that("no dupes", {
     }
   }
 })
+
+test_that("all between 1 and n", {
+  for (funcname in funcnames) {
+    for (i in 1:20) {
+      expect_true(all(funcs[[funcname]](i, i - 1, seq_len(i)) %in% seq_len(i)),
+                  label = funcname)
+    }
+  }
+})
