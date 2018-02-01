d77760a7ed9ccddd7ba1e41eca6a5558c85b7870
Kirill Müller <krlmlr+r@mailbox.org>
2018-02-01 18:27:33 +0100

    Rcpp autogen

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,18 +2,18 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 sample_int_crank <- function(n, size, prob) {
-    .Call('_wrswoR_sample_int_crank', PACKAGE = 'wrswoR', n, size, prob)
+    .Call(`_wrswoR_sample_int_crank`, n, size, prob)
 }
 
 sample_int_ccrank <- function(n, size, prob) {
-    .Call('_wrswoR_sample_int_ccrank', PACKAGE = 'wrswoR', n, size, prob)
+    .Call(`_wrswoR_sample_int_ccrank`, n, size, prob)
 }
 
 sample_int_expj <- function(n, size, prob) {
-    .Call('_wrswoR_sample_int_expj', PACKAGE = 'wrswoR', n, size, prob)
+    .Call(`_wrswoR_sample_int_expj`, n, size, prob)
 }
 
 sample_int_expjs <- function(n, size, prob) {
-    .Call('_wrswoR_sample_int_expjs', PACKAGE = 'wrswoR', n, size, prob)
+    .Call(`_wrswoR_sample_int_expjs`, n, size, prob)
 }
 
