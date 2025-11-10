0270ed2febb16f2aa272eebfd3759e12dc7cb2af
Kirill Müller <kirill@cynkra.com>
2025-11-10 07:12:32 +0100

    aspell

---
+++ .aspell/defaults.R
@@ -0,0 +1,6 @@
+# utils:::aspell_package(".")$Original |> saveRDS(".aspell/wrswoR.rds")
+
+Rd_files <- vignettes <- R_files <- description <-
+    list(encoding = "UTF-8",
+         language = "en",
+         dictionaries = c("en_stats", "wrswoR"))
